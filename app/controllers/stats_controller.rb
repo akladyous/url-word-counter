6456e1b2341860c47536f77f1d5fd1588e1a9eca
Boula Akladyous <akladyous@gmail.com>
2022-09-24 17:12:12 -0400

    added stats controller

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,17 @@
+class StatsController < ApplicationController
+  before_action :set_stats
+  def index
+  end
+
+  private
+
+  def stats_params
+    params.permit(:post_id)
+  end
+
+  def set_stats
+    @post = Post.friendly.find(stats_params[:post_id])
+    @stats = 10 #Stats.tokenize(stats_params[:post_id], exclude: ["some", "words"])
+  end
+
+end
